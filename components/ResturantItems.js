0847dc48754e99a1d48eed82bf443be797412fc8
Isaac <isaac231467@gmail.com>
2022-10-25 23:16:05 -0500

    forgot to make a new branch lol sorry

---
+++ components/ResturantItems.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ResturantItems() {
+  return (
+    <View>
+      <Text>ResturantItems</Text>
+    </View>
+  )
+};
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,29 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
-import SearchBar from './SearchBar'
+import { View, Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
+import React, {useState} from 'react';
+import SearchBar from './SearchBar';
+import ResturantItems from '../components/ResturantItems';
 
 export default function HomeScreen() {
+
+  const [] = useState('San Antonio');
+
+
   return (
-    <SafeAreaView>
+    <>
+    <SafeAreaView style={{backgroundColor: '#FFF8EA'}}>
+    <View style={styles.MainView}>
       <SearchBar />
+    </View> 
     </SafeAreaView>
+      <ScrollView>
+        <ResturantItems/>
+      </ScrollView>
+    </>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  MainView: {
+    padding: 15,
+  }
+})
